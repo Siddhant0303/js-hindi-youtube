8cb4f3648792d178978d40338c9cdd343fa0a4d1
Siddhant0303 <165934500+Siddhant0303@users.noreply.github.com>
2024-10-24 11:25:02 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+let name = "Siddhant"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => standalone value (representation of an empty value) (we can assign null value too)
+//undefined => variable is declared but i have not given any value
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
